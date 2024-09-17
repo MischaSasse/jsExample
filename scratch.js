994595800e20bf7c34d4a422f1b68c0fc2342351
Mischa Sasse <mmischaaaa4@gmail.com>
2024-09-17 19:48:18 +0200

    added basic js, added comments, changed src in html

--- scratch.js
+++ scratch.js
@@ -15,15 +15,59 @@
 * }
 */
 
-function changeTextColor(){
-    let currentStyle = document.body.style.color
-    console.log(currentStyle);
+
+function myObject() {
+    const obj = {
+        firstName: "Damiën",
+        lastName: "van den IJssel",
+        age: 23,
+    };
+    console.log(obj.firstName);
+    // let myName = obj[firstName]   Dit geeft een error
+    let myName = obj["firstName"] // Zie je het verschil met de lijn erboven?
+    console.log("name is "+myName);
     
-    const colorMap = { //zie hoe een map ook een object is?
-        "":"red",      //een map heeft standaard een base-case
-        red: "blue", 
-        blue: "yellow",
-        yellow: "red"
+}
+// myObject()
+
+function myArray() {
+    let nameArray = ["Damiën", "Lucas", "Mischa"];
+    nameArray.forEach((name) => {
+    console.log(name);
+  });
+}
+// myArray()
+
+function forLoop() {
+  let nameArray = ["Damiën", "Lucas", "Mischa"];
+  for (let index = 0; index < nameArray.length; index++) {
+      console.log(index + " " + nameArray[index]);
     }
-    document.body.style.color = colorMap[currentStyle] //set de textcolor van je body naar de value van je property
+}
+// forLoop()
+
+function whileLoop() {
+    let nameArray = ["Damiën", "Lucas", "Mischa"];
+    let i = 0; //je index variabel kan elke naam hebben die je maar wilt. Het is namelijk een variabel
+    //wel is het handig om hier normaal een i of index aan te geven
+    while (i < nameArray.length) {
+        console.log(nameArray[i]);
+    //after code
+    i++;
+}
+}
+// whileLoop()
+
+// HOE WORDT DIT TOEGEPAST?? Read below :>
+
+function changeTextColor() {
+  let currentTextColor = document.body.style.color;
+  const colorMap = {
+    "": "red",  
+    red: "blue",
+    blue: "yellow",
+    yellow: "green",
+  };
+  document.body.style.color = colorMap[currentTextColor]; //set de textcolor van je body naar de value van je property
+  //   document.getElementsByTagName('header')[0].style.backgroundColor = "purple"
 }
